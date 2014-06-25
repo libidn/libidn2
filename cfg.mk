bec266453e998081278bb0d396d829f53d2c6a6c
Simon Josefsson <simon@josefsson.org>
2014-06-25 15:52:38 +0200

    Fix syntax-check.

--- cfg.mk
+++ cfg.mk
@@ -34,6 +34,7 @@ exclude_file_name_regexp--sc_trailing_blank = ^(tests/IdnaTest.(txt|inc))|(doc/g
 exclude_file_name_regexp--sc_require_config_h = ^examples/.*\.c$$
 exclude_file_name_regexp--sc_require_config_h_first = ^examples/.*\.c$$
 exclude_file_name_regexp--sc_prohibit_strncpy = ^src/idn2.c$$
+exclude_file_name_regexp--sc_GPL_version = ^[^/]*.[ch]
 
 update-copyright-env = \
 	UPDATE_COPYRIGHT_USE_INTERVALS=2 \
