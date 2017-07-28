88505bc6a54acec9f5a7ea957286f98fa98eb61d
Tim Rühsen <tim.ruehsen@gmx.de>
2017-07-28 21:37:04 +0200

    Split punycode.c into encoding/decoding

--- lib/puny_encode.c
+++ lib/puny_encode.c
@@ -68,9 +68,7 @@
 #define punycode_success IDN2_OK
 #define punycode_overflow IDN2_PUNYCODE_OVERFLOW
 #define punycode_big_output IDN2_PUNYCODE_BIG_OUTPUT
-#define punycode_bad_input IDN2_PUNYCODE_BAD_INPUT
 #define punycode_encode _idn2_punycode_encode
-#define punycode_decode _idn2_punycode_decode
 
 /**********************************************************/
 /* Implementation (would normally go in its own .c file): */
@@ -86,19 +84,6 @@ enum { base = 36, tmin = 1, tmax = 26, skew = 38, damp = 700,
 
 /* basic(cp) tests whether cp is a basic code point: */
 #define basic(cp) ((punycode_uint)(cp) < 0x80)
-
-/* delim(cp) tests whether cp is a delimiter: */
-#define delim(cp) ((cp) == delimiter)
-
-/* decode_digit(cp) returns the numeric value of a basic code */
-/* point (for use in representing integers) in the range 0 to */
-/* base-1, or base if cp does not represent a value.          */
-
-static punycode_uint decode_digit(punycode_uint cp)
-{
-  return  cp - 48 < 10 ? cp - 22 :  cp - 65 < 26 ? cp - 65 :
-          cp - 97 < 26 ? cp - 97 :  base;
-}
 
 /* encode_digit(d,flag) returns the basic code point whose value      */
 /* (when used for representing integers) is d, which needs to be in   */
@@ -234,86 +219,5 @@ int punycode_encode(
   }
 
   *output_length = out;
-  return punycode_success;
-}
-
-/*** Main decode function ***/
-
-int punycode_decode(
-  size_t input_length,
-  const char input[],
-  size_t *output_length,
-  punycode_uint output[])
-{
-  punycode_uint n, out, i, max_out, bias, oldi, w, k, digit, t;
-  size_t b, j, in;
-
-  /* Initialize the state: */
-
-  n = initial_n;
-  out = i = 0;
-  max_out = *output_length > maxint ? maxint
-            : (punycode_uint) *output_length;
-  bias = initial_bias;
-
-  /* Handle the basic code points:  Let b be the number of input code */
-  /* points before the last delimiter, or 0 if there is none, then    */
-  /* copy the first b code points to the output.                      */
-
-  for (b = j = 0;  j < input_length;  ++j)  if (delim(input[j])) b = j;
-  if (b > max_out) return punycode_big_output;
-
-  for (j = 0;  j < b;  ++j) {
-    if (!basic(input[j])) return punycode_bad_input;
-    output[out++] = input[j];
-  }
-
-  /* Main decoding loop:  Start just after the last delimiter if any  */
-  /* basic code points were copied; start at the beginning otherwise. */
-
-  for (in = b > 0 ? b + 1 : 0;  in < input_length;  ++out) {
-
-    /* in is the index of the next ASCII code point to be consumed, */
-    /* and out is the number of code points in the output array.    */
-
-    /* Decode a generalized variable-length integer into delta,  */
-    /* which gets added to i.  The overflow checking is easier   */
-    /* if we increase i as we go, then subtract off its starting */
-    /* value at the end to obtain delta.                         */
-
-    for (oldi = i, w = 1, k = base;  ;  k += base) {
-      if (in >= input_length) return punycode_bad_input;
-      digit = decode_digit(input[in++]);
-      if (digit >= base) return punycode_bad_input;
-      if (digit > (maxint - i) / w) return punycode_overflow;
-      i += digit * w;
-      t = k <= bias /* + tmin */ ? tmin :     /* +tmin not needed */
-          k >= bias + tmax ? tmax : k - bias;
-      if (digit < t) break;
-      if (w > maxint / (base - t)) return punycode_overflow;
-      w *= (base - t);
-    }
-
-    bias = adapt(i - oldi, out + 1, oldi == 0);
-
-    /* i was supposed to wrap around from out+1 to 0,   */
-    /* incrementing n each time, so we'll fix that now: */
-
-    if (i / (out + 1) > maxint - n) return punycode_overflow;
-    n += i / (out + 1);
-    i %= (out + 1);
-
-    /* Insert n at position i of the output: */
-
-    /* not needed for Punycode: */
-    /* if (basic(n)) return punycode_bad_input; */
-    if (out >= max_out) return punycode_big_output;
-
-    memmove(output + i + 1, output + i, (out - i) * sizeof *output);
-    output[i++] = n;
-  }
-
-  *output_length = (size_t) out;
-  /* cannot overflow because out <= old value of *output_length */
   return punycode_success;
 }
