d8ae9649a901f39043fb7f2cee52a49a57718918
Simon Josefsson <simon@josefsson.org>
2021-05-11 18:30:06 +0200

    Fix copyright years.

--- src/blurbs.h
+++ src/blurbs.h
@@ -16,7 +16,7 @@
 */
 
 #define GREETING							\
-  "Copyright (C) 2011-2019  Simon Josefsson, Tim Ruehsen\n"				\
+  "Copyright (C) 2011-2021  Simon Josefsson, Tim Ruehsen\n"		\
   "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n" \
   "This is free software, and you are welcome to redistribute it\n"	\
   "under certain conditions; type `show c' for details.\n\n"
